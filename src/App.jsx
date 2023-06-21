3aedacd6c9188e9e21b9b4ca5c89a179b6573d18
Divyanshi <manraldivyanshi46@gmail.com>
2023-06-21 17:26:35 +0530

    final styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import CssBaseline from "@mui/material/CssBaseline";
 import TodoList from "./TodoList";
+import Navbar from "./Navbar";
 
 function App() {
   return <>
   {/* This strips away the browsers default styling */}
   <CssBaseline />
-  <h1>Todos</h1>
+  <Navbar />
   <TodoList/>
   </>;
 }
